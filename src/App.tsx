25cb2c2db3a52a5588e59a5f0c9122311c4531b7
Maorz <maorzehavi@outlook.com>
2024-08-06 19:10:03 +0300

    starting image

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,15 @@ import DishList from './features/DishList';
 import Home from './pages/Home';
 import background from './assets/images/HOME.jpg'
 import NavBar from './components/NavBar';
+import axios from 'axios';
+import apiClient from './utils/api';
 function App() {
+
+
   return (
     <div className='h-full '>
       <div className='bg-cover bg-center h-full min-h-screen' style={{ backgroundImage: `url(${background})` }}>
+        <img src='http://localhost:8080/api/image/5' alt="" />
         <NavBar/>
         <Routes>
           <Route path="/" element={<Home />} />
